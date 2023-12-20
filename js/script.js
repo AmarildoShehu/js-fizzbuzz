143dea7dc990931a9eb5af378ddbfcd9a091deca
Amarildo <amarildos@outloo.it>
2023-12-20 16:37:30 +0100

    added new div in mybox with js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 // 1. collegamento file html e script;
 console.log('js ok');
 
+// 3. riempio gli elementi tramite js;
+
+const mybox = document.getElementById('mybox');
+for(let i=0; i<50; i++){
+    const myNewDiv = document.createElement('div');
+    myNewDiv.classList.add('myNewDiv');
+    myNewDiv.textContent = Div i + 1;
+    mybox.appendChild(myNewDiv);
+}
